faf65266b8f68fc0dd801943e715f540c0efcc09
fiftekhar <iftekhar.f.19@gmail.com>
2022-10-19 19:43:20 -0400

    fixed clock with proper implementation

--- src/Clock.js
+++ src/Clock.js
@@ -1,13 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
+const root = ReactDOM.createRoot(
+    document.getElementById('root')
+);
   
-export default function Clock() {
+function tick() {
     const element = (
         <div>
             <h1>Hello, world!</h1>
             <h2>It is {new Date().toLocaleTimeString()}.</h2>
         </div>
     );
-    return element;
+    root.render(element);
 }
   
-// setInterval(tick, 1000);
+setInterval(tick, 1000);
+
+export default tick;
